a488310c97f54c18647077dc7ca6d982974b4011
Daniel Ruiz <druizca@mercadona.es>
2022-03-17 18:14:00 +0100

    Add failing test to find wardrobe combinations with two types of module

--- src/tests/test_wardrobe.py
+++ src/tests/test_wardrobe.py
@@ -12,3 +12,13 @@ def test_wall_is_filled_with_modules_of_the_same_type(wall, expected_combination
     combinations = find_wardrobe_combinations(wall=wall, available_modules=[Module(width=50)])
 
     assert combinations == expected_combinations
+
+
+def test_wall_is_filled_with_two_types_of_module():
+    combinations = find_wardrobe_combinations(wall=Wall(width=75), available_modules=[Module(width=50), Module(width=25)])
+
+    assert combinations == [
+        [Module(width=50), Module(width=25)],
+        [Module(width=25), Module(width=50)],
+        [Module(width=25), Module(width=25), Module(width=25)],
+    ]
