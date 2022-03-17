73b979a0cd2a1bb670532c772883a7e4d21718aa
Daniel Ruiz <druizca@mercadona.es>
2022-03-17 17:41:57 +0100

    Extract function that checks if a module fits in a wall

--- src/wardrobe.py
+++ src/wardrobe.py
@@ -15,9 +15,13 @@ def find_wardrobe_combinations(wall, available_modules):
     first_combination = []
 
     remaining_wall = wall
-    while module.width <= remaining_wall.width:
+    while _does_module_fit_in_wall(module, remaining_wall):
         first_combination.append(module)
         remaining_width = remaining_wall.width - module.width
         remaining_wall = Wall(width=remaining_width)
 
     return [first_combination]
+
+
+def _does_module_fit_in_wall(module, wall):
+    return module.width <= wall.width
