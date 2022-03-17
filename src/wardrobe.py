f66daf8103986b7ee99ab98e3a322e935ab954e6
Daniel Ruiz <druizca@mercadona.es>
2022-03-17 18:03:07 +0100

    First attempt to refactor the implementation to be recursive

--- src/wardrobe.py
+++ src/wardrobe.py
@@ -10,17 +10,22 @@ class Module:
     width: int = 0
 
 
-def find_wardrobe_combinations(wall, available_modules):
+def find_wardrobe_combinations(wall, available_modules, combinations=None):
     module = available_modules[0]
-    first_combination = []
 
-    remaining_wall = wall
-    while _does_module_fit_in_wall(module, remaining_wall):
-        first_combination.append(module)
-        remaining_width = remaining_wall.width - module.width
+    if combinations is None:
+        first_combination = []
+        return find_wardrobe_combinations(wall, available_modules, combinations=[first_combination])
+
+    combination = combinations[0]
+
+    if _does_module_fit_in_wall(module, wall):
+        combination.append(module)
+        remaining_width = wall.width - module.width
         remaining_wall = Wall(width=remaining_width)
+        return find_wardrobe_combinations(remaining_wall, available_modules, combinations)
 
-    return [first_combination]
+    return combinations
 
 
 def _does_module_fit_in_wall(module, wall):
