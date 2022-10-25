eed48d221395940497ddc89aedc1647adf94a2b2
Daniel Ruiz <druizca@mercadona.es>
2022-10-25 23:47:20 +0200

    Add successful, recursive implementation that finds all available options when there are two types of module available

--- src/wardrobe.py
+++ src/wardrobe.py
@@ -10,23 +10,51 @@ class Module:
     width: int = 0
 
 
-def find_wardrobe_combinations(wall, available_modules, combinations=None):
-    module = available_modules[0]
+def find_wardrobe_combinations(wall, available_modules):
+    wardrobe_combinations = []
 
-    if combinations is None:
-        first_combination = []
-        return find_wardrobe_combinations(wall, available_modules, combinations=[first_combination])
+    _find_module_combinations_recursively(wardrobe_combinations, wall, available_modules)
 
-    combination = combinations[0]
-
-    if _does_module_fit_in_wall(module, wall):
-        combination.append(module)
-        remaining_width = wall.width - module.width
-        remaining_wall = Wall(width=remaining_width)
-        return find_wardrobe_combinations(remaining_wall, available_modules, combinations)
-
-    return combinations
+    return wardrobe_combinations
 
 
-def _does_module_fit_in_wall(module, wall):
-    return module.width <= wall.width
+def _find_module_combinations_recursively(wardrobe_combinations, wall, available_modules):
+
+    for module in available_modules:
+        new_combination = [module]
+        if _can_find_next_combination(new_combination, wall, available_modules):
+            wardrobe_combinations.append(new_combination)
+
+
+def _can_find_next_combination(current_combination, wall, available_modules):
+
+    if not _does_combination_fit_in_the_wall(current_combination, wall):
+        return False
+
+    if (
+        _does_combination_fit_in_the_wall(current_combination, wall)
+        and _does_combination_fill_the_wall(current_combination, wall, available_modules)
+    ):
+        return True
+
+    for module in available_modules:
+        current_combination.append(module)
+
+        if _can_find_next_combination(current_combination, wall, available_modules):
+            return True
+
+        current_combination.pop()
+
+    return False
+
+
+def _does_combination_fill_the_wall(combination, wall, available_modules):
+    return not any(_does_module_fit_in_the_combination(module, combination, wall) for module in available_modules)
+
+
+def _does_combination_fit_in_the_wall(combination, wall):
+    return sum(module.width for module in combination) <= wall.width
+
+
+def _does_module_fit_in_the_combination(module, combination, wall):
+    return module.width + sum(module.width for module in combination) <= wall.width
